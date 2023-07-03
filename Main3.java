042195f3627ff953e0401d74ed55ef3c0735f11f
Riva@07 <136741490+Rivariyas@users.noreply.github.com>
2023-07-03 20:35:57 +0530

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,20 @@
+package camerarentalapp;
+
+/*import java.util.Scanner;
+public class Main3{
+	public static void main(String[] args) {
+		 CamRentApp camRentApp = new CamRentApp();
+		 camRentApp.displayWelcomeScreen();
+		 camRentApp.login();
+		 }
+		}
+*/
+
+import java.util.Scanner;
+public class Main3{
+	public static void main(String[] args) {
+		 CamRentApp camRentApp = new CamRentApp();
+		 camRentApp.displayWelcomeScreen();
+		 camRentApp.login();
+		 }
+		}

ae334c401f0f12cd73537ed3d694fdff4e3ffc6f
Riva@07 <136741490+Rivariyas@users.noreply.github.com>
2023-07-03 20:37:02 +0530

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -1,15 +1,5 @@
 package camerarentalapp;
 
-/*import java.util.Scanner;
-public class Main3{
-	public static void main(String[] args) {
-		 CamRentApp camRentApp = new CamRentApp();
-		 camRentApp.displayWelcomeScreen();
-		 camRentApp.login();
-		 }
-		}
-*/
-
 import java.util.Scanner;
 public class Main3{
 	public static void main(String[] args) {
